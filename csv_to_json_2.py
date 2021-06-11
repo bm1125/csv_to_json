d5174172c8f4e8552c6c5d52be5e8d5472b737b2
Sourcery AI <>
2021-06-11 12:25:36 +0000

    'Refactored by Sourcery'

--- csv_to_json_2.py
+++ csv_to_json_2.py
@@ -4,12 +4,12 @@ import sys
 import json
 
 def create_dict(keys, field_value):
-    if keys:
-        if isinstance(keys[0], tuple):
-            return {keys[0][0]:[create_dict(keys[1:], field_value)]}
-        return {keys[0]:create_dict(keys[1:], field_value)}
-    else:
+    if not keys:
         return field_value
+
+    if isinstance(keys[0], tuple):
+        return {keys[0][0]:[create_dict(keys[1:], field_value)]}
+    return {keys[0]:create_dict(keys[1:], field_value)}
 
 def update_dict(d_, keys, field_value):
     if isinstance(keys[0], tuple):
@@ -40,16 +40,16 @@ def update_list(d_, keys, field_value, idx):
         d_[keys[0]].append(create_dict(keys[1:], field_value))
 
 def islist(string):
+    digits = re.findall('\[\d+\]', string)
+    if not digits:
+        return string
+
+    digits = digits[0]
     translation = {91:None,
                   93:None}
-    digits = re.findall('\[\d+\]', string)
-    if digits:
-        digits = digits[0]
-        digits = int(digits.translate(translation))
-        clean_string = re.sub('\[\d+\]' ,'', string)
-        return clean_string, digits
-    else:
-        return string
+    digits = int(digits.translate(translation))
+    clean_string = re.sub('\[\d+\]' ,'', string)
+    return clean_string, digits
     
 def csv_to_json(filename, outfile_name, headers = None):
     with open(filename, 'r') as infile:
